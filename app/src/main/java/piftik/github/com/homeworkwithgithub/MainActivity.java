0267e9d2413dcbe8324ba6068612032d0dca1599
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-03 18:19:11 +0300

    added log

--- app/src/main/java/piftik/github/com/homeworkwithgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/homeworkwithgithub/MainActivity.java
@@ -2,17 +2,16 @@ package piftik.github.com.homeworkwithgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        comment in master branch
-        someMethod();
-    }
-    private void someMethod(){
-//        this method not working
+        Log.d(TAG, "some text");
     }
 }
